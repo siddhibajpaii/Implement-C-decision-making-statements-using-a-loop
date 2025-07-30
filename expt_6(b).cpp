13dfab378740cfae282779c5cf75b127aab2a792
Siddhi Bajpai <bajpaisiddhi20@gmail.com>
2025-07-30 23:13:00 +0530

    Create expt_6(b).cpp

---
+++ expt_6(b).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        std::cout << "SIT" << std::endl;
+    }
+    return 0;
+}
+
+//Output:
+//SIT
+//SIT
+//SIT
+//SIT
+//SIT
